ac5300c448cda2bd9a2647b7b5391455a9fe3ece
IrulJJ2 <vicbuild14@gmail.com>
2024-06-21 23:09:07 +0700

    Part 4 done

--- problem3/main.py
+++ problem3/main.py
@@ -1,9 +1,17 @@
 def cetak_table_perkalian(number):
     pattern = ""
+    for x in range(1,number+1):
+        print (x, end="\t")
+    print()
+    for y in range (1, number+1):
+        for z in range (1, number+1):
+            print (y*z, end ="\t")
+        print ("\n")
     return pattern
 
 if __name__ == '__main__':
     cetak_table_perkalian(9)
+
     """
      1 2 3 4 5 6 7 8 9
      2 4 6 8 10 12 14 16 18
--- problem5/main.py
+++ problem5/main.py
@@ -1,11 +1,19 @@
 def mean_median(array_input):
-    mean = 1.0
-    median = 1
+    if len(array_input) == 0:
+        return None
+    total = sum(array_input)
+    mean = total / len(array_input)
+    n = len(array_input)
+    if n % 2 == 0:
+        median = (array_input[n // 2 - 1] + array_input[n // 2]) / 2
+    else:
+        median = array_input[n // 2]
+    mean = round(mean, 1)
     return (mean, median)
 
 if __name__ == '__main__':
-    print(mean_median([1, 2, 3, 4])) # (2.5, 2.5)
-    print(mean_median([1, 2, 3, 4, 5])) # (3.0, 3)
-    print(mean_median([7, 8, 9, 13, 15])) # (10.4, 9)
-    print(mean_median([10, 20, 30, 40, 50])) # (30.0, 30)
-    print(mean_median([15, 20, 30, 60, 120])) # (49.0, 30)
+    print(mean_median([1, 2, 3, 4]))      # Expected output: (2.5, 2.5)
+    print(mean_median([1, 2, 3, 4, 5]))   # Expected output: (3.0, 3)
+    print(mean_median([7, 8, 9, 13, 15])) # Expected output: (10.4, 9)
+    print(mean_median([10, 20, 30, 40, 50])) # Expected output: (30.0, 30)
+    print(mean_median([15, 20, 30, 60, 120])) # Expected output: (49.0, 30)
