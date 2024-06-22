06d55a231422b08a96703298b752fdc4346045ee
Irul <vicbuild14@gmail.com>
2024-06-22 16:30:12 +0700

    Create Arithmetic Operators.py

---
+++ Hackerrank/Arithmetic Operators.py
@@ -0,0 +1,20 @@
+#Task
+#The provided code stub reads two integers from STDIN,  and . Add code to print three lines where:
+#The first line contains the sum of the two numbers.
+#The second line contains the difference of the two numbers (first - second).
+#The third line contains the product of the two numbers.
+
+#Link : https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+a = int(input())
+b = int(input())
+    
+c = a + b
+d = a - b
+e = a * b
+    
+print (c)
+print (d)
+print (e)
+    
+
