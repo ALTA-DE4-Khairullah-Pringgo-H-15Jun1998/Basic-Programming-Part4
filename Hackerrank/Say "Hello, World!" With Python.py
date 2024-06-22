71588fd418e89cfd6e8cdf9260d4cca60c8b858e
Irul <vicbuild14@gmail.com>
2024-06-22 16:23:15 +0700

    Update Say "Hello, World!" With Python.py

--- Hackerrank/Say "Hello, World!" With Python.py
+++ Hackerrank/Say "Hello, World!" With Python.py
@@ -1,1 +1,11 @@
+#soal : Here is a sample line of code that can be executed in Python:
+#print("Hello, World!")
+#You can just as easily store a string as a variable and then print it to stdout:
+#my_string = "Hello, World!"
+#print(my_string)
+#The above code will print Hello, World! on your screen. Try it yourself in the editor below!
+
+#Link soal : https://www.hackerrank.com/challenges/py-hello-world/problem?isFullScreen=true
+
+print("Hello, World!")
 
